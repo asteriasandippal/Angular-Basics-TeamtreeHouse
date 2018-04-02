97ee5a5386963ab428d31b3faed1bf91fbd366e9
asteriasandippal <sandip.pal@asteria.co.in>
2018-04-02 19:01:48 +0530

    Added Entry lists use service

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Entry } from '../shared/entry.model';
 @Component({
     selector: 'app-entry',
     templateUrl: 'entry.component.html',
@@ -7,21 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class EntryComponent {
-    title: string = "My First Photo";
-    photo: string = "http://via.placeholder.com/800x500?text=Angular";
-    description: string = "A Description of My First Photo";
-    comments: any[] = [
-        {
-            name: "Sandip", 
-            comment: "A comment"
-        },
-        {
-            name: "Rayel", 
-            comment: "A comment"
-        },
-        {
-            name: "Gopal", 
-            comment: "A comment"
-        }
-    ]
+    @Input() entry: Entry;
 }
