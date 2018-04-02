cb43bf46ea458580ed8a762ab240d0f7bab343ea
asteriasandippal <sandip.pal@asteria.co.in>
2018-04-02 16:57:47 +0530

    Added ngFor ngif

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -10,4 +10,18 @@ export class EntryComponent {
     title: string = "My First Photo";
     photo: string = "http://via.placeholder.com/800x500?text=Angular";
     description: string = "A Description of My First Photo";
+    comments: any[] = [
+        {
+            name: "Sandip", 
+            comment: "A comment"
+        },
+        {
+            name: "Rayel", 
+            comment: "A comment"
+        },
+        {
+            name: "Gopal", 
+            comment: "A comment"
+        }
+    ]
 }
