48c21c079bcab63290b6b03646bb399f48f3e1f2
asteriasandippal <sandip.pal@asteria.co.in>
2018-04-02 19:27:01 +0530

    Added Comment Form

---
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-entry-comment-form',
+    templateUrl: 'entry-comment-form.component.html',
+    styleUrls: ['entry-comment-form.component.css']
+})
+
+export class EntryCommentFormComponent {
+
+}
